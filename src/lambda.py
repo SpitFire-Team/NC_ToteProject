e579aff343198719784220474a9eaed76da852db
imightbebruno <bruno.sterzabaggio@gmail.com>
2025-05-28 12:32:55 +0100

    makefile creating cache

--- src/lambda.py
+++ src/lambda.py
@@ -10,6 +10,7 @@ host = os.getenv("HOST")
 port = os.getenv("PORT")
 database = os.getenv("DATABASE")
 
+
 def db_connection():
     try:
         conn = pg8000.connect(
@@ -23,14 +24,15 @@ def db_connection():
     except pg8000.InterfaceError as e:
         raise Exception(f"Database connection failed: {e}")
 
+
 def lambda_handler(event, context):
     try:
         conn = db_connection()
         cursor = conn.cursor()
-    
+
     except Exception as e:
         raise Exception(f"Database connection failed: {e}")
-    
+
     finally:
         cursor.close()
         conn.close()
