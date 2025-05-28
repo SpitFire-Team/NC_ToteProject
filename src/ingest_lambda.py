1caa52909504167f14312f04fa98a56433018dac
imightbebruno <bruno.sterzabaggio@gmail.com>
2025-05-28 16:32:42 +0100

    fixing issues with branch

--- src/ingest_lambda.py
+++ src/ingest_lambda.py
@@ -2,15 +2,20 @@ from dotenv import load_dotenv
 import pg8000
 import os
 
-load_dotenv()
-
-user = os.getenv("USER")
-password = os.getenv("PASSWORD")
-host = os.getenv("HOST")
-port = os.getenv("PORT")
-database = os.getenv("DATABASE")
-
 def db_connection():
+    '''
+    Grabs environment variables and uses them to create a database connection.
+    Returns:
+        pg8000 database connection.
+    Raises:
+        exception if the connection fails due to interface errors.
+    '''
+    load_dotenv()
+    user = os.getenv("USER")
+    password = os.getenv("PASSWORD")
+    host = os.getenv("HOST")
+    port = int(os.getenv("PORT"))
+    database = os.getenv("DATABASE")
     try:
         conn = pg8000.connect(
             user=user,
@@ -22,15 +27,16 @@ def db_connection():
         return conn
     except pg8000.InterfaceError as e:
         raise Exception(f"Database connection failed: {e}")
-
+    
 def lambda_handler(event, context):
     try:
         conn = db_connection()
         cursor = conn.cursor()
-    
+
     except Exception as e:
         raise Exception(f"Database connection failed: {e}")
     
     finally:
         cursor.close()
         conn.close()
+
