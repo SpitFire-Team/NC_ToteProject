d950030c4418d08f9b52520c8f14c934265d9fb4
RichardAW <Richard.Armstrong.Wood@gmail.com>
2025-05-31 18:39:42 +0100

    add layers to lambda function - tested on console and working

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -1,35 +1,25 @@
 resource "null_resource" "create_dependencies" {
   provisioner "local-exec" {
-    command = "pip install -r ${path.module}/../src/layer/dependencies/lambda_requirements.txt -t ${path.module}/../src/layer/dependencies/python"
+    command = "pip install -r ${path.module}/../${var.path_layer}/lambda_requirements.txt -t ${path.module}/../${var.path_layer}/dependencies/python"
   }
-
   triggers = {
-    dependencies = filemd5("${path.module}/../requirements.txt")
+    dependencies = filemd5("${path.module}/../${var.path_layer}/lambda_requirements.txt")
   }
+}
+
+data "archive_file" "lambda_requirements" {
+    type        = "zip"
+    source_dir  = "${path.module}/../${var.path_layer}/dependencies"
+    output_path = "${path.module}/../${var.path_layer}/layer.zip"
+    depends_on = [null_resource.create_dependencies]
 }
 
 resource "aws_lambda_layer_version" "layer" {
   layer_name = "lambda_layer"
-  filename = "${path.module}/../src/layer/layer.zip"
+  filename = "${path.module}/../${var.path_layer}/layer.zip"
   compatible_architectures = ["x86_64"]
   compatible_runtimes = [var.runtime]
+  depends_on = [data.archive_file.lambda_requirements]
 }
 
 
-# data "archive_file" "lambda_requirements" {
-#     type        = "zip"
-#     source_file = "${path.module}/../src/layer/lambda_requirements.txt"
-#     output_path = "${path.module}/../src/layer/layer.zip"
-# }
-
-# data "archive_file" "layer_code" {
-#   type        = "zip"
-#   output_path = "${path.module}/../src/layer/layer.zip"
-#   source_dir  = "${path.module}/../src/dependencies"
-# }
-
-# resource "aws_lambda_layer_version" "dependencies" {
-#   layer_name = "requests_library_layer"
-#   s3_bucket  = aws_s3_object.lambda_layer.bucket
-#   s3_key     = aws_s3_object.lambda_layer.key
-# }
