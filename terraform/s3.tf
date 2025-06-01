942b869f61252efed4ced26afe4179a5678d0b3b
RichardAW <Richard.Armstrong.Wood@gmail.com>
2025-06-01 22:36:38 +0100

    add s3 bucket for layer code so it deploys from aws

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -17,20 +17,22 @@ resource "aws_s3_bucket" "processed_bucket" {
 }
 
 
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket_prefix = "lambda-bucket-"
+resource "aws_s3_bucket" "code_bucket" {
+  bucket_prefix = "code-bucket-"
 
   tags = {
-    Name        = "Lambda storage bucket"
+    Name        = "Code storage bucket"
     Environment = "Dev"
   }
 }
 
-# resource "aws_s3_object" "lambda_layer_bucket" {
-#   bucket = aws_s3_bucket.code_bucket.bucket
-#   key    = "layer/layer.zip"
-#   source = data.archive_file.layer_code.output_path
-#   etag   = filemd5(data.archive_file.layer_code.output_path)
-#   depends_on = [ data.archive_file.layer_code ]
-# }
+resource "aws_s3_object" "lambda_layer" {
+  bucket = aws_s3_bucket.code_bucket.bucket
+  key    = "layer/layer.zip"
+  source = data.archive_file.layer_code.output_path
+  etag   = filemd5(data.archive_file.layer_code.output_path)
+  depends_on = [ data.archive_file.layer_code ]
+}
+
+
 
