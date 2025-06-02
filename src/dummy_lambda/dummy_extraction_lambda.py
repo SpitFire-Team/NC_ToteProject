ed343f9c5c5190da28efd04600b1d3515f698aa3
newissa1 <neymaurassa@gmail.com>
2025-06-02 19:08:44 +0300

    add IAM roles to dummy functions to work

---
+++ src/dummy_lambda/dummy_extraction_lambda.py
@@ -0,0 +1,6 @@
+
+
+
+def lambda_handler(event, context):
+    return print("Dummy_extraction_lambda_activated")
+    
---
+++ src/dummy_lambda/dummy_load_lambda.py
@@ -0,0 +1,5 @@
+
+
+
+def lambda_handler(event, context):
+    return print("Dummy_load_lambda_activated")
