bf72126489e4a67fb7d996b8a2f449160180e87a
RichardAW <Richard.Armstrong.Wood@gmail.com>
2025-06-02 09:55:11 +0100

    rearrange files 2nd try

--- terraform/layer.tf
+++ terraform/layer.tf
@@ -28,6 +28,5 @@ resource "aws_s3_object" "lambda_layer" {
   bucket = aws_s3_bucket.code_bucket.bucket
   key    = "layer/layer.zip"
   source = data.archive_file.layer_code.output_path
-  # etag   = filemd5(data.archive_file.layer_code.output_path)
   depends_on = [data.archive_file.layer_code]
 }
