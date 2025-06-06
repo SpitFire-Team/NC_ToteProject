5f7eac72ab591c0467e6578b9aef6483f984a945
bryo-phyte <bryonysjones@gmail.com>
2025-06-06 16:52:38 +0100

    add docstring for dataframe_modification

---
+++ src/transformation_lambda/dataframe_modification.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def dataframe_modification():
+    """This function takes a list of dictionaries
+    containing dataframe values, 
+    removes columns 'last_updated'
+    and 'created_at' from the dataframes 
+    and returns a list of dictionaries containing the modified dataframes.
+
+    Arguments: A list of dictionaries in the format [{table_name: dataframe}, {table_name_2: dataframe_2}]
+    Returns: A list of dictionaries in the format [{table_name: modified_dataframe}, {table_name_2: modified_dataframe_2}]
+    """
+    pass
+
---
+++ test/test_dataframe_modification.py
@@ -0,0 +1,4 @@
+from src.transformation_lambda.dataframe_modification import dataframe_modification
+
+def test_dataframe_modification_returns_list_of_dicts():
+    
