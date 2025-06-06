fdc21ab1a7d6c6ab14af65c53c8e2ab317cbbd6c
spatialista <spatialista@outlook.com>
2025-06-06 10:36:43 +0100

    adding clean up of get_items_from_database black/ flake8 checks

--- test/test_get_items_aws_integration.py
+++ test/test_get_items_aws_integration.py
@@ -1,17 +1,13 @@
 import boto3
 import pytest
-import time
 import psycopg2
 import os
 from dotenv import load_dotenv
-import datetime
 from datetime import datetime, timezone
 
 # from src.extraction_lambda.extraction_lambda import db_connection
 from src.extraction_lambda.get_items_from_database import (
-    set_latest_updated_time,
     check_database_updates,
-    query_all_tables,
 )
 
 # load in variables from .env file. Below is configured to find .env in project root, while file is run from test/dir (specifically when root is one level above test dir). Test .dir location with: print("loading .env from:", dotenv_path)
@@ -37,7 +33,7 @@ def real_db_connection():
     totesys_db_connection = psycopg2.connect(
         user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT, database=DB_NAME
     )
-    cursor = totesys_db_connection.cursor()
+    # cursor = totesys_db_connection.cursor()
     return totesys_db_connection
 
 
