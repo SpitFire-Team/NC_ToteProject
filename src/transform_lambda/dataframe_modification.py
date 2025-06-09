dc827984f15e497e8d414ca4e6decce6b0eb776c
bryo-phyte <bryonysjones@gmail.com>
2025-06-09 11:21:51 +0100

    add test returns list for dataframe modification function

--- src/transform_lambda/dataframe_modification.py
+++ src/transform_lambda/dataframe_modification.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def dataframe_modification():
+def dataframe_modification(list_of_dicts):
     """This function takes a list of dictionaries
     containing dataframe values,
     removes columns 'last_updated'
@@ -11,4 +11,9 @@ def dataframe_modification():
     Arguments: A list of dictionaries in the format [{table_name: dataframe}, {table_name_2: dataframe_2}]
     Returns: A list of dictionaries in the format [{table_name: modified_dataframe}, {table_name_2: modified_dataframe_2}]
     """
-    pass
+    modified_list=[]
+    for table_dict in list_of_dicts:
+        print(table_dict)
+        new_dict = {key:"test" for key in table_dict}
+        modified_list.append(new_dict)                  
+    return modified_list
--- test/test_dataframe_modification.py
+++ test/test_dataframe_modification.py
@@ -1,5 +1,26 @@
-from src.transformation_lambda.dataframe_modification import dataframe_modification
+from src.transform_lambda.dataframe_modification import dataframe_modification
+import pandas as pd
+import json
+from io import StringIO
 
+string_1 ={"sales_order_id": [2,3],
+  "created_at": ["2022-11-03 14:20:52.186000","2022-11-03 14:20:52.188000"],
+  "last_updated": ["2022-11-03 14:20:52.186000","2022-11-03 14:20:52.188000"],
+  "design_id": [3,4],
+  "staff_id": [19,10],
+  "counterparty_id": [8,4],
+  "units_sold": [42972,65839],
+  "unit_price": ["3.94","2.91"],
+  "currency_id": [2,3],
+  "agreed_delivery_date": ["2022-11-07","2022-11-06"],
+  "agreed_payment_date": ["2022-11-08","2022-11-07"],
+  "agreed_delivery_location_id": [8,19]}
+
+dataframe_1 = pd.DataFrame(string_1)
+
+test_dict_list= [{"sales_order": dataframe_1}]
 
 def test_dataframe_modification_returns_list_of_dicts():
-    pass
+    test_dict_list= [{"sales_order": dataframe_1}]
+    assert type(dataframe_modification(test_dict_list)) == list
+    assert "sales_order" in dataframe_modification(test_dict_list)[1].keys
