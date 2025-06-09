c3378125ac1bfda728741ecd772c0b51bbee2bc8
bryo-phyte <bryonysjones@gmail.com>
2025-06-09 16:01:47 +0100

    "Bringing up to date with main"

--- src/transform_lambda/dataframe_modification.py
+++ src/transform_lambda/dataframe_modification.py
@@ -2,8 +2,8 @@ def dataframe_modification(list_of_dicts):
     """This function takes a list of dictionaries
     containing table name keys and dataframe values,
     removes columns 'last_updated'
-    and 'created_at' from the dataframes
-    and returns a list of dictionaries containing the modified dataframes.
+    and 'created_at' from the dataframes,
+    it then returns a list of dictionaries containing the modified dataframes.
 
     Arguments: A list of dictionaries in the format [{table_name: dataframe}, {table_name_2: dataframe_2}]
     Returns: A list of dictionaries in the format [{table_name: modified_dataframe}, {table_name_2: modified_dataframe_2}]
@@ -12,7 +12,9 @@ def dataframe_modification(list_of_dicts):
     for item in list_of_dicts:
         table_name = list(item.keys())[0]
         target_dataframe = item[table_name]
-        modified_dataframe= target_dataframe.drop(["created_at", "last_updated"], axis=1)
+        modified_dataframe = target_dataframe.drop(
+            ["created_at", "last_updated"], axis=1
+        )
         new_dict = {table_name: modified_dataframe}
         modified_list.append(new_dict)
     return modified_list
