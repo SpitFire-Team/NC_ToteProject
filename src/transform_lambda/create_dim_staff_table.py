8c687e7945fb3e3e5ced8f4d8dde4f933aef83b6
newissa1 <neymaurassa@gmail.com>
2025-06-09 12:32:39 +0300

    rename folder and create new files

---
+++ src/transform_lambda/create_dim_staff_table.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+
+
+def transform_staff():
+    # Load raw data into a DataFrame
+    raw_staff_data = [ ]
+
+df = pd.DataFrame(raw_staff_data)
+
+#Renaming the columns 
+
+df.rename(columns={
+    "id": "staff_id",
+    "department_id": "department_id"  
+}, inplace=True)
+
+
+#  Create full_name field
+
+df["full_name"] = df["first_name"] + " " + df["last_name"]
+
+# Remove sensitive or unused columns like password.
+
+df.drop (columns=["password"], inplace=True)
+
+def transform_currency():
+    
