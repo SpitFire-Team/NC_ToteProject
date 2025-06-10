9cf8aa4d253e3ebed26ea63fb04c6570990847b3
bryo-phyte <bryonysjones@gmail.com>
2025-06-10 09:40:08 +0100

    add file structure for load_to_warehouse function, add warehouse connection

---
+++ src/load_lambda/load_to_warehouse.py
@@ -0,0 +1,12 @@
+
+def load_to_warehouse():
+    """
+    This function will load transformed data into appropriate tables in
+    a pre-prepared data warehouse
+    Inputs: 
+    A list of dictionaries in the format [{table_name: dataframe}, {table_name_2: dataframe_2}]
+    """
+    pass
+
+#
+#CONNECT TO SQL, write dataframe
---
+++ src/load_lambda/warehouse_connection.py
@@ -0,0 +1,27 @@
+def db_connection():
+    """
+    Grabs environment variables and uses them to create a database connection.
+    Returns:
+        pg8000 database connection.
+    Raises:
+        exception if the connection fails due to interface errors.
+    """
+    load_dotenv()
+
+    user = os.getenv("USER")
+    password = os.getenv("PASSWORD")
+    host = os.getenv("HOST")
+    port = int(os.getenv("PORT"))
+    database = os.getenv("DATABASE")
+
+    try:
+        conn = pg8000.connect(
+            user=user,
+            password=password,
+            host=host,
+            port=port,
+            database=database,
+        )
+        return conn
+    except pg8000.InterfaceError as e:
+        raise Exception(f"Database connection failed: {e}")
