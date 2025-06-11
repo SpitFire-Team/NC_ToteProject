c6fe6a71e6aa33d1e9ea8d1a3f978eab516009da
spatialista <spatialista@outlook.com>
2025-06-11 11:38:44 +0100

    adding latest version of WIP transform lambda handler

--- src/transform_lambda/transform_lambda.py
+++ src/transform_lambda/transform_lambda.py
@@ -1,69 +1,81 @@
-import boto3
-import pandas
-from pprint import pprint
+# import boto3 - uncomment for AWS
 from src.transform_lambda.read_json_to_dataframe import read_json_to_dataframe
 from src.transform_lambda.dataframe_modification import dataframe_modification
-from src.transform_lambda.create_dim_staff_table import transform_staff_and_department_tables
+from src.transform_lambda.create_dim_staff_table import (
+    transform_staff_and_department_tables,
+)
+from src.transform_lambda.transform_data_parquet_s3 import (
+    transform_data_to_parquet_on_s3,
+)
 from src.utils.aws_utils import make_s3_client, get_bucket_name
 
 
 def lambda_handler(event, context):
 
-    date_time_str_last_ingestion = event[0]["last_ingested_str"] # date_time_str coming from step function and extraction lambda
+    date_time_str_last_ingestion = event[0]["last_ingested_str"]
 
-    # date_time_str_last_ingestion = (
-    #     "09-06-2025_07:46"  # example date time string for testing
-    # )
-    
-
-    # s3 client
+    # create s3 client
     s3_client = make_s3_client()
 
     """ uncomment for AWS implementation:
+    # create s3 client
     s3_client = boto3.client(
         "s3"
     )"""
 
-    # get full s3 bucket name
+    # retreive full s3 bucket name
     bucket_prefix = "ingested-data"
     bucket_name = get_bucket_name(s3_client, bucket_prefix)
 
-    # call read_json_to_datafram which reads s3 json datafiles and returns list of dicts mapping table names to Pandas dataframes
-    ingested_data = read_json_to_dataframe(s3_client, bucket_name, date_time_str_last_ingestion)
-    print("in func, ingested_data len >>>>", len(ingested_data))
-    print("in func, ingested_data >>>>", ingested_data)
+    # read_json_to_dataframe reads s3 json datafiles and returns list of dicts mapping table names to Pandas dataframes
+    ingested_data = read_json_to_dataframe(
+        s3_client, bucket_name, date_time_str_last_ingestion
+    )
+    print("ingested_data len >>>>", len(ingested_data))
+    print("ingested_data >>>>", ingested_data)
 
-    # pass the list of dictionaries to dataframe_modification which removes unnecessary columns
+    # pass the list of dicts to dataframe_modification which removes unnecessary columns
     modified_data = dataframe_modification(ingested_data)
-    print("in func, modified_data >>>>", modified_data)
+    print("modified_data_len >>>>", len(modified_data))
+    print("modified_data >>>>", modified_data)
 
-    # from modified dataframes, select specifically the 'staff' and 'department' dataframes to be passed to create_dim_staff_table
-
+    # from the newly modified dataframes, select the 'staff' and 'department' dataframes to be passed to create_dim_staff_table
     for dict in modified_data:
         for key, df in dict.items():
             if key == "staff":
                 staff_df = df
-                print("staff_df", staff_df)
+                # print("staff_df >>>>", staff_df)
             if key == "department":
                 department_df = df
-                print("department_df", department_df)
+                # print("department_df >>>>", department_df)
 
-    # create_dim_staff_table returns combined dim_staff table
+    # create_dim_staff_table returns combined dim_staff dataframe
     combined_dim_staff = transform_staff_and_department_tables(staff_df, department_df)
-    print("combined_dim_staff", combined_dim_staff)
-    
+    # print("combined_dim_staff >>>>", combined_dim_staff)
+
     # add dim_staff table dictionary to list of dicts
+    modified_data.append({"dim_staff": combined_dim_staff})
+    print("modified_data_len2 >>>>", len(modified_data))
+    print("modified_data2 >>>>", modified_data)
+
     # remove staff and department from list of dicts
-    # pass newly transformed list of dicts to transform_data_underscore_s3
+    for dict in modified_data:
+        dict.pop("staff", None)
+        dict.pop("department", None)
+
+    # remove empty dicts
+    modified_data = [dict for dict in modified_data if dict]
+
+    print("modified_data_len3 >>>>", len(modified_data))
+    print("modified_data3 >>>>", modified_data)
+    for dict in modified_data:
+        print("Updated modified data table >>>", dict.keys())
+
+    # pass newly transformed list of dicts to transform_data_parquet_s3
+    transformed_data = transform_data_to_parquet_on_s3(
+        s3_client, modified_data, date_time_str_last_ingestion
+    )
+    print("transformed data >>>>", transformed_data)
+
     # return event: datetime string (for load lambda handler)
-
     return event
-
-
-    # staff and department list of dicts (not needed)
-    # staff_and_department = []
-    # for dict in modified_data:
-    #     for key in dict:
-    #         if key == "staff" or key == "department":
-    #             staff_and_department.append(dict)
-    # print ("in func, staff_and_department", staff_and_department)
--- test/test_transform_lambda_handler.py
+++ test/test_transform_lambda_handler.py
@@ -8,50 +8,87 @@ def dummy_event_timenow():
     date_time_last_ingestion = get_path_date_time_string()
     return [{"last_ingested_str": date_time_last_ingestion}]
 
+
 @pytest.fixture
 def dummy_event_fixedtime():
-    return [{"last_ingested_str": '06-06-2025_23:55'}] # this timestamp includes files with created_at and last_updated columns
+    return [
+        {"last_ingested_str": "06-06-2025_23:55"}
+    ]  # this timestamp includes files with created_at and last_updated columns
 
+
+@pytest.mark.skip("Skipped testing directly to AWS due to time constraints")
 class TestLambdaHandler:
     @pytest.mark.skip
     def test_datetime_string_passed_into_lambda_handler(self, dummy_event_timenow):
         result = lambda_handler(dummy_event_timenow, [{}])
         assert result == dummy_event_timenow
-    
+
     @pytest.mark.skip
-    def test_json_to_dataframe_does_not_error_when_created_at_and_last_updated_columns_not_present(self):
-        last_ingestion_time = [{"last_ingested_str": '06-06-2025_23:54'}] # this timestamp includes files with no created_at and last_updated columns
+    def test_json_to_dataframe_does_not_error_when_created_at_and_last_updated_columns_not_present(
+        self,
+    ):
+        last_ingestion_time = [
+            {"last_ingested_str": "06-06-2025_23:54"}
+        ]  # this timestamp includes files with no created_at and last_updated columns
         result = lambda_handler(last_ingestion_time, [{}])
+        assert result is not None
         # test with mock up s3 bucket
 
     @pytest.mark.skip
     def test_json_to_dataframe_returns_list_of_dicts(self, dummy_event_fixedtime):
         result = lambda_handler(dummy_event_fixedtime, [{}])
+        assert result is not None
         # test with mock up s3 bucket - query data structure
 
     @pytest.mark.skip
     def test_json_to_dataframe_returns_modified_columns(self, dummy_event_fixedtime):
         result = lambda_handler(dummy_event_fixedtime, [{}])
+        assert result is not None
         # test with mock up s3 bucket - show column number reducing by 2
-    
+
     @pytest.mark.skip
-    def test_json_to_dataframe_returns_staff_and_department_dicts_only(self, dummy_event_fixedtime):
+    def test_json_to_dataframe_returns_staff_and_department_dicts_only(
+        self, dummy_event_fixedtime
+    ):
         result = lambda_handler(dummy_event_fixedtime, [{}])
+        assert result is not None
         # test with mock up s3 bucket - show new list output containing list of dicts with staff and department only
-    
+
     @pytest.mark.skip
-    def test_json_to_dataframe_extracts_staff_and_department_dataframes(self, dummy_event_fixedtime):
+    def test_json_to_dataframe_extracts_staff_and_department_dataframes(
+        self, dummy_event_fixedtime
+    ):
         result = lambda_handler(dummy_event_fixedtime, [{}])
+        assert result is not None
         # test with mock up s3 bucket - show staff and department dataframes created
 
     @pytest.mark.skip
-    def test_json_to_dataframe_returns_combined_dim_staff_table(self, dummy_event_fixedtime):
+    def test_json_to_dataframe_returns_combined_dim_staff_table(
+        self, dummy_event_fixedtime
+    ):
         result = lambda_handler(dummy_event_fixedtime, [{}])
+        assert result is not None
         # test with mock up s3 bucket - show combined dim staff table output
 
-    def test_json_to_dataframe_returns_combined_dim_staff_table(self, dummy_event_fixedtime):
+    @pytest.mark.skip
+    def test_json_to_dataframe_returns_adds_dim_staff_to_list_of_dicts(
+        self, dummy_event_fixedtime
+    ):
         result = lambda_handler(dummy_event_fixedtime, [{}])
-        # test with mock up s3 bucket - show combined dim staff table output
+        assert result is not None
+        # test with mock up s3 bucket - show modified_data includes dim_staff dataframe
 
+    @pytest.mark.skip
+    def test_json_to_dataframe_returns_removes_staff_and_department_from_list_of_dicts(
+        self, dummy_event_fixedtime
+    ):
+        result = lambda_handler(dummy_event_fixedtime, [{}])
+        assert result is not None
+        # test with mock up s3 bucket - show modified_data removes staff and department dfs along with empty dicts
 
-    
+    def test_json_to_dataframe_returns_transforms_list_of_dicts_to_parquet(
+        self, dummy_event_fixedtime
+    ):
+        result = lambda_handler(dummy_event_fixedtime, [{}])
+        assert result is not None
+        # test with mock up s3 bucket - show list of dicts transformed as parquet
