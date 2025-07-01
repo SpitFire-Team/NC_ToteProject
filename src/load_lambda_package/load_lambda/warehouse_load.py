63985fc4baf178ed33fb1a46dfbd600997cb8133
RichardAW <Richard.Armstrong.Wood@gmail.com>
2025-07-01 10:30:36 +0100

    add commented ew version of load to warehouse loop

--- src/load_lambda_package/load_lambda/warehouse_load.py
+++ src/load_lambda_package/load_lambda/warehouse_load.py
@@ -33,3 +33,38 @@ def load_to_warehouse_loop(dict_list, conn):
             df.to_sql(table_name, con=conn, if_exists="append", index=False)
     except Exception as e:
         raise Exception(f"Could not append to table: {e}")
+
+
+## below is updated version modified for deploying lambdas branch. 
+
+
+# def load_to_warehouse_loop(dict_list, conn):
+#     """
+#     #     This function will load transformed data into appropriate tables in
+#     #     a pre-prepared data warehouse
+#     #     Inputs:
+#     #     A list of dictionaries in the format [{table_name: dataframe}, {table_name_2: dataframe_2}]
+#     #"""
+#     items_loaded = 0
+#     # pprint(dict_list)
+#     inspector = inspect(conn)
+
+#     for item in dict_list:
+#         table_name = list(item.keys())[0]
+#         df = item[table_name]
+#         if inspector.has_table(table_name, schema='project_team_09'):
+#             df.to_sql(
+#                 name=table_name,
+#                 con=conn,
+#                 if_exists="append",
+#                 index=False,
+#                 schema="project_team_09"
+#             )
+#             items_loaded += 1
+#         else:
+#             print(f"Table '{table_name}' does not exist. Skipping.")
+            
+
+#     return f"items loaded {items_loaded}"
+#     # except Exception as e:
+#     #     raise Exception(f"Could not append to table: {e}")
