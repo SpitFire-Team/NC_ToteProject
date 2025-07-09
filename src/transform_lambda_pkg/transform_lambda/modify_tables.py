8d8efc063b186365da060e2b272481f12a503af2
RichardAW <Richard.Armstrong.Wood@gmail.com>
2025-07-09 17:17:38 +0100

    rename to modify tables, and refactor to add fixtures to testing. Also, move create_none_merge_datastructure out of merge data

---
+++ src/transform_lambda_pkg/transform_lambda/modify_tables.py
@@ -0,0 +1,55 @@
+def dataframe_modification(list_of_dicts):
+    """This function takes a list of dictionaries
+    containing table name keys and dataframe values,
+    removes columns 'last_updated'
+    and 'created_at' from the dataframes,
+    it then returns a list of dictionaries containing the modified dataframes.
+
+    Arguments: A list of dictionaries in the format [{table_name: dataframe}, {table_name_2: dataframe_2}]
+    Returns: A list of dictionaries in the format [{table_name: modified_dataframe}, {table_name_2: modified_dataframe_2}]
+    """
+    modified_list = []
+    for item in list_of_dicts:
+        table_name = list(item.keys())[0]
+        target_dataframe = item[table_name]
+        modified_dataframe = target_dataframe.drop(
+            ["created_at", "last_updated"], axis=1, errors="ignore"
+        )
+        new_dict = {table_name: modified_dataframe}
+        modified_list.append(new_dict)
+    return modified_list
+
+def create_non_merged_datastructure(tables, table_names):
+
+    if type(tables) != list:
+        raise Exception("Tables should be type list")
+    elif tables: 
+        for item in tables:
+            if type(item) != dict:
+                raise Exception("Tables should a list of dictionaries")
+    else: 
+        raise Exception("Tables should not be empty")
+    
+    
+    if type(table_names) != list:
+        raise Exception("Table names should be type list")
+    elif table_names: 
+        for item in table_names:
+            if type(item) != str:
+                raise Exception("Table names should a list of strings")
+    else: 
+        raise Exception("Table names should not be empty")
+    
+    return_data_structure = []
+    for table in tables:
+        table_dict = {}
+
+        table_name = list(table.keys())[0]
+        if table_name in table_names:
+            table_dict[table_name] = table[table_name]
+            table_dict["col_list"] = star_schema_ref[transform_table_names[table_name]]
+
+        if table_dict:
+            return_data_structure.append(table_dict)
+
+    return return_data_structure
